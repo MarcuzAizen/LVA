20a51760797c05b23316bd4d34ad0dc026bcc87d
Nikko Lumahang <kertnik@gmail.com>
2020-10-14 13:02:56 +0800

    model, factory, migration & seeder for user added

---
+++ database/migrations/2020_10_13_180409_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('role_id')->default(5)->constrained();
+            $table->string('first_name', 45);
+            $table->string('middle_name', 45)->nullable();
+            $table->string('last_name', 45);
+            $table->string('suffix', 10)->nullable();
+            $table->char('sex', 1);
+            $table->date('birthdate');
+            $table->string('contact_number', 11);
+            $table->string('purok', 45);
+            $table->string('barangay', 45);
+            $table->string('city', 45);
+            $table->string('province', 45);
+            $table->string('username', 25);
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unique(['username', 'deleted_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}

2f254d999cfdaa534d7eba8bc7d3ee820d987de3
Nikko Lumahang <kertnik@gmail.com>
2020-10-14 15:37:25 +0800

    api resource route for role added

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\RoleController;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('users/teachers', [UserController::class, 'getTeachers'])->name('users.teachers');
+Route::get('users/teachers/search', [UserController::class, 'searchTeachers'])->name('users.search_teachers');
+
 Route::apiResource('roles', RoleController::class);
+Route::apiResource('users', UserController::class);

39666e14a1d34e6f57ede4247f0e592994bc7588
MarcuzAizen <reymark4991mutia@gmail.com>
2020-11-03 17:32:05 +0800

    Add api controller (SubjectController)

---
+++ app/Http/Controllers/API/SubjectController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Subject;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreSubjectRequest;
+use App\Http\Requests\UpdateSubjectRequest;
+use App\Http\Resources\SubjectResource;
+
+class SubjectController extends Controller
+{
+    public function index()
+    {
+        $subjects = Subject::latest()->paginate(10);
+        return SubjectResource::collection($subjects);
+    }
+
+    public function store(StoreSubjectRequest $request)
+    {
+        Subject::create([
+            'code' => $request->code,
+            'description' => $request->description,
+            'unit' => $request->unit, 
+        ]);
+        return response()->json([
+            'success' => true,
+            'message' => 'Subject Successfully Created',
+        ], 201);
+    }
+
+    public function show(Subject $subject)
+    {
+        return new SubjectResource($subject);
+    }
+
+    public function update(UpdateSubjectRequest $request, Subject $subject)
+    {
+        $subject->update($request->validated());
+        return response()->json([
+            'success' => true,
+            'message' => 'Subject Successfully Updated',
+        ]);
+    }
+
+    public function destroy(Subject $subject)
+    {
+        if($subject->delete())
+        {
+            return response()->json([
+                'success' => true,
+                'message' => 'Subject Successfully Deleted',
+            ]);
+        }
+    }
+}

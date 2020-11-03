975b313ca013ec647642f8c63e3fbde10c64c363
MarcuzAizen <reymark4991mutia@gmail.com>
2020-11-03 17:29:06 +0800

    add resource (SubjectResource), Add Requests ( StoreSubjectREquest, UpdateSubjectRequest) for api Subject Controller

---
+++ app/Http/Requests/StoreSubjectRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreSubjectRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+    
+    public function rules()
+    {
+        return [
+            'code' => ['required', 'string', Rule::unique('subjects')->whereNull('deleted_at')],
+            'description' => ['required', 'string', 'max:50'],
+            'unit' => ['required', 'integer', 'min:1', 'max:2'],
+        ];
+    }
+}

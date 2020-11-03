73ad2189818b46f59ec90a2b07ecaf03a255a70c
MarcuzAizen <reymark4991mutia@gmail.com>
2020-11-03 13:32:07 +0800

    Add api Resource Controller

---
+++ app/Http/Controllers/API/AcadYearController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\AcadYear;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreAcadYearRequest;
+use App\Http\Requests\UpdateAcadYearRequest;
+use App\Http\Resources\AcadYearResource;
+
+class AcadYearController extends Controller
+{
+    
+    public function index()
+    {
+        $acadYears = AcadYear::latest()->paginate(10);
+        return AcadYearResource::collection($acadYears);
+    }
+
+   
+    public function store(StoreAcadYearRequest $request)
+    {
+        AcadYear::create([
+            'start' => $request->start, 
+            'end' => $request->start + 1,
+        ]);
+        return response()->json([
+            'success' => true,
+            'message' => 'Academic Year Successfully Created',
+        ], 201);
+    }
+
+  
+    public function show(AcadYear $acadYear)
+    {
+        return new AcadYearResource($acadYear);
+    }
+
+   
+    public function update(UpdateAcadYearRequest $request, AcadYear $acadYear)
+    {
+        $acadYear->update($request->validated());
+        return response()->json([
+            'success' => true,
+            'message' => 'Academic Year Successfully Updated',
+        ]);
+    }
+
+    public function destroy(AcadYear $acadYear)
+    {
+        if($acadYear->delete())
+        {
+            return response()->json([
+                'success' => true,
+                'message' => 'Academic Year Successfully Deleted',
+            ]);
+        }
+    }
+}

b3b29c21a2b1ef2c4ebca40e5d725815c67cde71
MarcuzAizen <reymark4991mutia@gmail.com>
2020-11-05 23:11:46 +0800

    Edited some parameters in both store and update for  track and section requests. deleted SectionTrackRequest form

--- app/Http/Requests/StoreSectionRequest.php
+++ app/Http/Requests/StoreSectionRequest.php
@@ -6,24 +6,15 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreSectionRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
+            'track_id'=> ['nullable', 'integer', 'min:1'],
             'name' => ['required', 'string', 'min:3', 'max:45'],
             'capacity' => ['required', 'integer', 'min:1', 'max:50'],
         ];
--- app/Http/Requests/UpdateTrackRequest.php
+++ app/Http/Requests/UpdateTrackRequest.php
@@ -6,27 +6,17 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateTrackRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
             'name' => ['required', 'string', 'min:3', 'max:45'],
-            'description' => ['required', 'string', 'max:50'],
-            'grade_level' => ['required', 'integer', 'min:1', 'max:12'],
+            'description' => ['required', 'string'],
+            'grade_level' => ['required', 'integer', 'min:7', 'max:12'],
         ];
     }
 }

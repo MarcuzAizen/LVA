c1a385f3784a2415018f70fa5875efdbb1ea43d7
MarcuzAizen <reymark4991mutia@gmail.com>
2020-11-06 10:59:44 +0800

    Added controller for prospectus api

---
+++ app/Http/Controllers/API/ProspectusController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Prospectus;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProspectusResource;
+use App\Http\Requests\StoreProspectusRequest;
+use App\Http\Requests\UpdateProspectusRequest;
+
+class ProspectusController extends Controller
+{
+    public function index()
+    {
+        $prospectuses = Prospectus::with(['track', 'subject'])->latest()->paginate(10);
+        return ProspectusResource::collection($prospectuses);
+    }
+
+    public function store(StoreProspectusRequest $request)
+    {
+        Prospectus::create($request->validated());
+        return response()->json([
+            'success' => true,
+            'message' => 'Prospectus successfully created!'
+        ], 201);
+    }
+
+    public function show(Prospectus $prospectus)
+    {
+        return new ProspectusResource($prospectus->load(['track', 'subject']));
+    }
+
+    public function update(UpdateProspectusRequest $request, Prospectus $prospectus)
+    {
+        $prospectus->update($request->validated());
+        return response()->json([
+            'success' => true,
+            'message' => 'Prospectus successfully updated!'
+        ]);
+    }
+
+    public function destroy(Prospectus $prospectus)
+    {
+        $prospectus->tracks()->detach();
+        $prospectus->subjects()->detach();
+        if ($prospectus->delete()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Prospectus successfully deleted!'
+            ]);
+        }
+    }
+}

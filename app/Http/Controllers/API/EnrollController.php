f4416c0c31e95be378136cd638005d7249b592bc
Nikko Lumahang <kertnik@gmail.com>
2020-11-06 12:51:44 +0800

    api resource controller for enroll crud added

---
+++ app/Http/Controllers/API/EnrollController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreEnrollRequest;
+use App\Http\Requests\UpdateEnrollRequest;
+use App\Http\Resources\EnrollResource;
+use App\Models\Enroll;
+use Illuminate\Http\Request;
+
+class EnrollController extends Controller
+{
+    public function index()
+    {
+        $enrolls = Enroll::with(['registrar', 'student', 'acadYear', 'section'])
+            ->latest()->paginate(10);
+        return EnrollResource::collection($enrolls);
+    }
+
+    public function store(StoreEnrollRequest $request)
+    {
+        Enroll::create($request->validated());
+        return response()->json([
+            'success' => true,
+            'message' => 'Student successfully enrolled!'
+        ], 201);
+    }
+
+    public function show(Enroll $enroll)
+    {
+        return new EnrollResource($enroll);
+    }
+
+    public function update(UpdateEnrollRequest $request, Enroll $enroll)
+    {
+        $enroll->update($request->validated());
+        return response()->json([
+            'success' => true,
+            'message' => 'Enroll data successfully updated!'
+        ]);
+    }
+
+    public function destroy(Enroll $enroll)
+    {
+        if ($enroll->delete()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Enroll data successfully deleted!'
+            ]);
+        }
+    }
+}

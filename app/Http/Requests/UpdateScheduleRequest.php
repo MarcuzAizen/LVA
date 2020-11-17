b8a3de68db69d75db416e09d78486086cc515dcd
Nikko Lumahang <kertnik@gmail.com>
2020-11-17 19:46:37 +0800

    form request validation for schedule crud added

---
+++ app/Http/Requests/UpdateScheduleRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateScheduleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'acad_year_id' => ['required', 'integer', 'min:1'],
+            'teacher_id' => ['required', 'integer', 'min:1'],
+            'section_id' => ['required', 'integer', 'min:1'],
+            'prospectus_id' => ['required', 'integer', 'min:1'],
+            'day' => ['required', 'string', 'min:1', 'max:1', Rule::in(['M', 'T', 'W', 'H', 'F'])],
+            'time_start' => ['required', 'date_format:H:i'],
+            'time_end' => ['required', 'date_format:H:i', 'after:time_start']
+        ];
+    }
+}

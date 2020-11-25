0c77e5e0dfd30cd653beba9c4c5efb34ed12f624
MarcuzAizen <reymark4991mutia@gmail.com>
2020-11-25 23:33:52 +0800

    Created components for teacher module

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import { registrarHome } from './modules/registrar';
+import { teacherHome } from './modules/teacher';
+
 Vue.use(VueRouter);
 
 const routes = [
-    registrarHome
+    registrarHome,
+    teacherHome
 ];
 
 export default new VueRouter({

39fc8f14bdf88480838cf1a4872262aec7f7988a
MarcuzAizen <reymark4991mutia@gmail.com>
2020-11-26 12:22:01 +0800

    changed brand name into teacher from registrar

--- resources/views/teacher/includes/sidebar.blade.php
+++ resources/views/teacher/includes/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-light-success elevation-4">
     <router-link :to="{ name: 'teacher.home' }" class="brand-link navbar-success">
         <img src="/images/app/apple-touch-icon.png" alt="LVA Logo" class="brand-image img-circle" style="opacity: .8">
-        <span class="brand-text font-weight-normal text-dark">Registrar</span>
+        <span class="brand-text font-weight-normal text-dark">Teacher</span>
     </router-link>
 
     {{-- Sidebar --}}

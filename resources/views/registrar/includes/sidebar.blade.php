e3a3003a9bd23b89f08e3755d8a82ac01f199709
MarcuzAizen <reymark4991mutia@gmail.com>
2020-11-26 12:21:12 +0800

    changed back to registrar "was mistakenly edited to teacher".

--- resources/views/registrar/includes/sidebar.blade.php
+++ resources/views/registrar/includes/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-light-warning elevation-4">
     <router-link :to="{ name: 'registrar.home' }" class="brand-link navbar-warning">
         <img src="/images/app/apple-touch-icon.png" alt="LVA Logo" class="brand-image img-circle" style="opacity: .8">
-        <span class="brand-text font-weight-normal text-dark">Teacher</span>
+        <span class="brand-text font-weight-normal text-dark">Registrar</span>
     </router-link>
 
     {{-- Sidebar --}}

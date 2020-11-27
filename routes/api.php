1b147f3cd39863bb82da7ba9ee1dbca1fd034601
Nikko Lumahang <kertnik@gmail.com>
2020-11-27 13:39:44 +0800

    moved section api routes to registrar.php

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,6 @@ Route::apiResource('student-remarks', StudentRemarkController::class);
 Route::apiResource('students', StudentController::class);
 Route::apiResource('guardians', GuardianController::class);
 Route::apiResource('acad-years', AcadYearController::class);
-Route::apiResource('sections', SectionController::class);
 Route::apiResource('prospectuses', ProspectusController::class);
 Route::apiResource('enrolls', EnrollController::class);
 Route::apiResource('schedules', ScheduleController::class); 
--- routes/registrar.php
+++ routes/registrar.php
@@ -4,13 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\HomeController;
 use App\Http\Controllers\API\SubjectController;
 use App\Http\Controllers\API\TrackController;
+use App\Http\Controllers\API\SectionController;
 
 Route::prefix('api')->group(function () {
     Route::get('subjects/search', [SubjectController::class, 'search'])->name('subjects.search');
     Route::get('tracks/search', [TrackController::class, 'search'])->name('tracks.search');
+    Route::get('tracks/all', [TrackController::class, 'getTracksNoPagination'])->name('tracks.no_pagination');
+    Route::get('sections/search', [SectionController::class, 'search'])->name('sections.search');
 
     Route::apiResource('subjects', SubjectController::class); 
     Route::apiResource('tracks', TrackController::class);
+    Route::apiResource('sections', SectionController::class);
 });
 
 Route::get('/', function () {

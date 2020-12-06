5827e009f8ccdb01487580a9f75a0f9d13786fbd
Nikko Lumahang <kertnik@gmail.com>
2020-12-06 16:53:07 +0800

    added mutator for start attribute in AcadYear model

--- app/Models/AcadYear.php
+++ app/Models/AcadYear.php
@@ -11,4 +11,12 @@ class AcadYear extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['start', 'end'];
+
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function setStartAttribute($value)
+    {
+        $this->attributes['start'] = $value;
+        $this->attributes['end'] = $value + 1;
+    }
 }

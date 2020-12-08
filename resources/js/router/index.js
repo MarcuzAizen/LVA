5af47c569a5be9ac4f92597ac73e4e7e9ddf7c78
Nikko Lumahang <kertnik@gmail.com>
2020-12-08 12:59:43 +0800

    Create Principal Dashboard Fixes #49

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import { registrarHome, subject, track, section, schoolYear } from './modules/registrar';
+import { principalHome } from './modules/principal';
 import { teacherHome } from './modules/teacher';
 
 Vue.use(VueRouter);
@@ -11,6 +12,7 @@ const routes = [
     track,
     section,
     schoolYear,
+    principalHome,
     teacherHome
 ];
 
--- routes/principal.php
+++ routes/principal.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\HomeController;
 
+// TODO: add api routes for principal here
+
 Route::get('/', function () {
     return redirect()->route('principal.home');
 });
 Route::get('home', [HomeController::class, 'principal'])->name('home');
+Route::get('/{any}', [HomeController::class, 'principal'])->where('any', '.*');

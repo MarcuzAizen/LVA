b41a914eba29e3b4ea489402a45d333560649cf6
Nikko Lumahang <kertnik@gmail.com>
2020-12-13 19:52:11 +0800

    moved api routes for teacher to principal.php

--- routes/principal.php
+++ routes/principal.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\HomeController;
+use App\Http\Controllers\API\UserController;
 
-// TODO: add api routes for principal here
+Route::prefix('api')->group(function() {
+    Route::get('users/teachers', [UserController::class, 'getTeachers'])->name('users.teachers');
+    Route::get('users/teachers/search', [UserController::class, 'searchTeachers'])->name('users.search_teachers');
+    Route::post('users', [UserController::class, 'store'])->name('users.store');
+    Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+});
 
 Route::get('/', function () {
     return redirect()->route('principal.home');

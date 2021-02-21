684507e8c15187bd8db0b65cd174188ce0fa79f3
Kert Rey Nikko S. Lumahang <kertnik@gmail.com>
2021-02-21 21:46:18 +0800

    moved api routes to enrollemen_officer.php

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\RoleController;
 use App\Http\Controllers\API\UserController;
-use App\Http\Controllers\API\StudentRemarkController;
-use App\Http\Controllers\API\StudentController;
 use App\Http\Controllers\API\GuardianController;
-use App\Http\Controllers\API\EnrollController;
 use App\Http\Controllers\API\GradeController;
 use App\Http\Controllers\API\AttendanceController;
 /*
@@ -25,13 +22,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('students/{student}/add-guardian', [StudentController::class, 'addGuardian'])->name('students.add_guardian');
-Route::delete('students/{student}/remove-guardian/{guardian}', [StudentController::class, 'removeGuardian'])->name('students.remove_guardian');
-
 Route::apiResource('roles', RoleController::class);
 Route::apiResource('users', UserController::class);
-Route::apiResource('student-remarks', StudentRemarkController::class);
 Route::apiResource('guardians', GuardianController::class);
-Route::apiResource('enrolls', EnrollController::class);
 Route::apiResource('grades', GradeController::class);
 Route::apiResource('attendances', AttendanceController::class);
--- routes/enrollment_officer.php
+++ routes/enrollment_officer.php
@@ -7,16 +7,20 @@ use App\Http\Controllers\API\AcadYearController;
 use App\Http\Controllers\API\SectionController;
 use App\Http\Controllers\API\StudentRemarkController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\EnrollController;
 
 Route::prefix('api')->group(function() {
     Route::get('students/search', [StudentController::class, 'search'])->name('students.search');
     Route::post('students/check-lrn', [StudentController::class, 'checkLrn'])->name('students.checkLrn');
+    Route::post('students/{student}/add-guardian', [StudentController::class, 'addGuardian'])->name('students.add_guardian');
+    Route::delete('students/{student}/remove-guardian/{guardian}', [StudentController::class, 'removeGuardian'])->name('students.remove_guardian');
     Route::get('acad-years/all', [AcadYearController::class, 'getAllAcadYears'])->name('acad_years.all');
     Route::get('sections/all', [SectionController::class, 'getAllSections'])->name('sections.all');
-    Route::get('student-remarks', [StudentRemarkController::class, 'index'])->name('student_remarks.index');
     Route::get('user', [UserController::class, 'getCurrentUser'])->name('current_user');
 
     Route::apiResource('students', StudentController::class);
+    Route::apiResource('enrolls', EnrollController::class);
+    Route::apiResource('student-remarks', StudentRemarkController::class);
 });
 
 Route::get('/', function () {
